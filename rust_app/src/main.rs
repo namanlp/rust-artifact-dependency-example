c19e9e5622a1c8832a5e79d80c8a9aecb75d4fc5
Naman Garg <ngarg1270@gmail.com>
2025-04-04 01:50:49 +0530

    Remove debug code

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -1,11 +1,4 @@
 fn main() {
-    println!("Printing works!");
-    // Try to find CARGO_BIN env variables, but can't find any
-    for (key, value) in std::env::vars() {
-        if key.starts_with("CARGO_BIN") {
-            println!("{key}: {value}");
-        }
-    }
     let status = std::process::Command::new(env!("CARGO_BIN_FILE_LIBHELLO_side")).status().unwrap();
 
     if !status.success() {
